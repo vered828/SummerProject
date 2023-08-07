e9c81b03d22a8324ae52dcbdeadc6568236dc69c
YaakovHatam <6986221+YaakovHatam@users.noreply.github.com>
2023-08-07 16:57:16 +0300

    little fixes

--- ExpensesManagement.js
+++ ExpensesManagement.js
@@ -1,4 +1,5 @@
 const optionsArray = ["Taxi", "Food", "Clothes"];
+
 const selectBox = document.getElementById("type");
 optionsArray.forEach(type => {
   const optionElement = document.createElement("option");
@@ -10,12 +11,13 @@ var dataLoggedUser = JSON.parse(localStorage.getItem("loggedUser")) || {};
 var username = dataLoggedUser.userName;
 const expensesKey = `${username}Expenses`;
 var ExpensesKey = JSON.parse(localStorage.getItem(expensesKey)) || [];
-document.getElementById("add").addEventListener("click", function (){
-    var data = {
-        sum: document.getElementById('sum').value,
-        kind: document.getElementById('type').value,
-        date: document.getElementById('date').value,
-    };
-    ExpensesKey.push(data);
-    localStorage.setItem(expensesKey, JSON.stringify(ExpensesKey));
+
+document.getElementById("add").addEventListener("click", function () {
+  var data = {
+    sum: document.getElementById('sum').value,
+    kind: document.getElementById('type').value,
+    date: document.getElementById('date').value,
+  };
+  ExpensesKey.push(data);
+  localStorage.setItem(expensesKey, JSON.stringify(ExpensesKey));
 });
