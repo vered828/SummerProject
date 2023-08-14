6e0f12789fb4edebdc41c92b0cf5b034fac538a5
vered828 <128394938+vered828@users.noreply.github.com>
2023-08-14 11:44:34 +0300

    ExpensesManagement.js

--- ExpensesManagement.js
+++ ExpensesManagement.js
@@ -1,5 +1,4 @@
 const optionsArray = ["Taxi", "Food", "Clothes"];
-
 const selectBox = document.getElementById("type");
 optionsArray.forEach(type => {
   const optionElement = document.createElement("option");
@@ -11,13 +10,12 @@ var dataLoggedUser = JSON.parse(localStorage.getItem("loggedUser")) || {};
 var username = dataLoggedUser.userName;
 const expensesKey = `${username}Expenses`;
 var ExpensesKey = JSON.parse(localStorage.getItem(expensesKey)) || [];
-
-document.getElementById("add").addEventListener("click", function () {
-  var data = {
-    sum: document.getElementById('sum').value,
-    kind: document.getElementById('type').value,
-    date: document.getElementById('date').value,
-  };
-  ExpensesKey.push(data);
-  localStorage.setItem(expensesKey, JSON.stringify(ExpensesKey));
+document.getElementById("add").addEventListener("click", function (){
+    var data = {
+        sum: document.getElementById('sum').value,
+        kind: document.getElementById('type').value,
+        date: document.getElementById('date').value,
+    };
+    ExpensesKey.push(data);
+    localStorage.setItem(expensesKey, JSON.stringify(ExpensesKey));
 });
