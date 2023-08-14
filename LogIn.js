d88d0362cc31185dd497cf431f2ffc2b1a0ef209
vered828 <128394938+vered828@users.noreply.github.com>
2023-08-14 12:03:38 +0300

    Update LogIn.js

--- LogIn.js
+++ LogIn.js
@@ -1,1 +1,44 @@
+var dataPasswordsArray = JSON.parse(localStorage.getItem("passwordsData")) || [];
+var dataLoggedUser = JSON.parse(localStorage.getItem("loggedUser")) || {};
 
+function handleSubmit(event) {
+    event.preventDefault();
+    const vlifn = document.getElementById('InputUN').value
+    const vlips = document.getElementById('InputPassword').value
+
+    var dataUsersArray = JSON.parse(localStorage.getItem("usersData")) || [];
+
+    const isDataExists = dataUsersArray.some(data => data.userName === vlifn && data.password === vlips);
+    if (isDataExists) {
+        const checkbox = document.getElementById("Check");
+        if(checkbox.checked){
+            var data = {
+                userName: vlifn,
+                password: vlips,
+            };
+            dataPasswordsArray.push(data);
+            localStorage.setItem("passwordsData", JSON.stringify(dataPasswordsArray));
+        }
+        dataUsersArray.forEach(function(userdata){
+          if (userdata.userName === vlifn){
+            localStorage.setItem("loggedUser", JSON.stringify(userdata));
+          }
+        });
+        window.location.href= "index.html";
+      } else {
+        document.getElementById('mess').innerHTML = 'Username or password incorrect';
+      }
+}
+document.getElementById("libt").addEventListener("click", handleSubmit);
+
+function checkUserExists() {
+  const vlifn2 = document.getElementById('InputUN').value
+  const vlips2 = document.getElementById('InputPassword')
+  
+  const existingUser = dataPasswordsArray.find((user) => user.userName == vlifn2)
+  if (existingUser) {
+    //צריך להוסיף שאם המשתמש כבר קיים במערך ה"זכור אותי" הוא לא יוסיף שוב
+    vlips2.value = existingUser.password;
+  }
+}
+document.getElementById("InputUN").addEventListener("input", checkUserExists);
